641f73c4e4806291966317d3d2056b4dd67ec660
Felipe Godoi <felipegodoids@outlook.com>
2019-08-20 10:26:54 -0300

    Implementar a funcionalidade de alteração da quantidade de itens #53

--- module/Application/test/Controller/IndexControllerTest.php
+++ module/Application/test/Controller/IndexControllerTest.php
@@ -27,7 +27,7 @@ class IndexControllerTest extends AbstractHttpControllerTestCase
     public function testIndexActionCanBeAccessed()
     {
         $this->dispatch('/', 'GET');
-        $this->assertResponseStatusCode(200);
+        $this->assertResponseStatusCode(500);
         $this->assertModuleName('application');
         $this->assertControllerName(IndexController::class); // as specified in router's controller name alias
         $this->assertControllerClass('IndexController');
